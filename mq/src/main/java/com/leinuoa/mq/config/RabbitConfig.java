bed59c2f27db77cc489f7e3e481b659dd9833cc8
helei <2067955108@qq.com>
2021-06-04 11:02:56 +0800

    rabbitmq 配置调整

--- mq/src/main/java/com/leinuoa/mq/config/RabbitConfig.java
+++ mq/src/main/java/com/leinuoa/mq/config/RabbitConfig.java
@@ -10,35 +10,34 @@ public class RabbitConfig {
     public static final String QUEUE_INFORM_EMAIL = "queue_inform_email";
     public static final String QUEUE_INFORM_SMS = "queue_inform_sms";
     public static final String EXCHANGE_TOPICS_INFORM = "exchange_topics_inform";
-    public static final String ROUTINGKEY_EMAIL = "inform.#.email.#";
-    public static final String ROUTINGKEY_SMS = "inform.#.sms.#";
+    public static final String ROUTING_KEY_EMAIL = "inform.#.email.#";
+    public static final String ROUTING_KEY_SMS = "inform.#.sms.#";
     //声明交换机
     @Bean(EXCHANGE_TOPICS_INFORM)
-    public Exchange EXCHANGE_TOPICS_INFORM(){
+    public Exchange exchangeTopicsInform(){
         //durable(true) 持久化，mq重启后交换机还在
         return ExchangeBuilder.topicExchange(EXCHANGE_TOPICS_INFORM).durable(true).build();
     }
 
     //声明队列
     @Bean(QUEUE_INFORM_EMAIL)
-    public Queue QUEUE_INFORM_EMAIL(){
+    public Queue queueInformEmail(){
         return new Queue(QUEUE_INFORM_EMAIL);
     }
     @Bean(QUEUE_INFORM_SMS)
-    public Queue QUEUE_INFORM_SMS(){
+    public Queue queueInformSms(){
         return new Queue(QUEUE_INFORM_SMS);
     }
 
     //绑定交换机和队列
     @Bean
-    public Binding BINDING_QUEUE_INFORM_EMAIL(@Qualifier(QUEUE_INFORM_EMAIL)Queue queue,
-                                              @Qualifier(EXCHANGE_TOPICS_INFORM)Exchange exchange ){
-        return BindingBuilder.bind(queue).to(exchange).with(ROUTINGKEY_EMAIL).noargs();
+    public Binding bindingQueueInformEmail(@Qualifier(QUEUE_INFORM_EMAIL)Queue queue, @Qualifier(EXCHANGE_TOPICS_INFORM)Exchange exchange ){
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY_EMAIL).noargs();
     }
     @Bean
-    public Binding BINDING_QUEUE_INFORM_SMS(@Qualifier(QUEUE_INFORM_SMS)Queue queue,
+    public Binding bindingQueueInformSms(@Qualifier(QUEUE_INFORM_SMS)Queue queue,
                                               @Qualifier(EXCHANGE_TOPICS_INFORM)Exchange exchange ){
-        return BindingBuilder.bind(queue).to(exchange).with(ROUTINGKEY_SMS).noargs();
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY_SMS).noargs();
     }
 
 }
